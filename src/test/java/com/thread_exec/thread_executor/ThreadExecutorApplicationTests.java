0eac0ec432b7d91e44db015e7df72da9082d1c73
onekb.twotb@gmail.com <Jiitukadu001@gmail.com>
2024-05-29 23:10:43 +0530

    commit initial development

---
+++ src/test/java/com/thread_exec/thread_executor/ThreadExecutorApplicationTests.java
@@ -0,0 +1,15 @@
+package com.thread_exec.thread_executor;
+
+import com.thread_exec.thread_executor.model.Customer;
+import com.thread_exec.thread_executor.service.CustomerService;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@RequiredArgsConstructor
+class ThreadExecutorApplicationTests {
+
+}
+
---
+++ src/test/java/com/thread_exec/thread_executor/service/HelperTest.java
@@ -0,0 +1,7 @@
+package com.thread_exec.thread_executor.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelperTest {
+
+}
